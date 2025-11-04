99e387017df51cfd5c294c59656fcb9c9fa75eb4
xuba <xuba@cisco.com>
2025-11-04 10:47:51 +0800

    Add patch for parquet map key handling in Arrow 22.0.0

--- thirdparty/download-thirdparty.sh
+++ thirdparty/download-thirdparty.sh
@@ -529,6 +529,10 @@ if [[ -d $TP_SOURCE_DIR/$ARROW_SOURCE ]] ; then
         patch -p1 < $TP_PATCH_DIR/arrow-16.1.0-use-zstd-1.5.7.patch
         touch $PATCHED_MARK
     fi
+    if [ ! -f $PATCHED_MARK ] && [ $ARROW_SOURCE = "arrow-apache-arrow-22.0.0" ] ; then
+        patch -p1 < $TP_PATCH_DIR/arrow-22.0.0-parquet-map-key.patch
+        touch $PATCHED_MARK
+    fi
     cd -
     echo "Finished patching $ARROW_SOURCE"
 fi
